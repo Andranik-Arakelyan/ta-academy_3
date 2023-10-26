c7481f1ec0f64875bea0824e75a67c17af7d003c
Andranik Arakelyan <andranikarakelyan365@gmail.com>
2023-10-27 00:50:51 +0400

    Finished Homework_4

--- js/lesson_4/src/App.tsx
+++ js/lesson_4/src/App.tsx
@@ -1,31 +1,42 @@
-import React from 'react';
-import './App.css';
-import {arraySum, arrayFilter, getUserNames} from './utils';
+// import React from "react";
+import { personNames } from "./task";
+import "./App.css";
+// import { arraySum, arrayFilter, getUserNames } from "./utils";
 
 function App() {
-    const array = [1, 2, 3, 4, 5];
-    const usersArray = [
-        {
-            name: "Peter",
-            surname: 'Jackson',
-            age: 20,
-        },
-        {
-            name: "Mary",
-            age: "30",
-        }
-    ]
-    const sum = arraySum(array);
-    const filter = arrayFilter(array, item => item % 2 === 0);
-    const userNamesArray = getUserNames(usersArray);
-
+  const showPersonNames = () => {
     return (
+      <ul>
+        {personNames.map((person, index) => (
+          <li key={index}>{person}</li>
+        ))}
+      </ul>
+    );
+  };
+  //   const array = [1, 2, 3, 4, 5];
+  //   const usersArray = [
+  //     {
+  //       name: "Peter",
+  //       surname: "Jackson",
+  //       age: 20,
+  //     },
+  //     {
+  //       name: "Mary",
+  //       age: "30",
+  //     },
+  //   ];
+  //   const sum = arraySum(array);
+  //   const filter = arrayFilter(array, (item) => item % 2 === 0);
+  //   const userNamesArray = getUserNames(usersArray);
+
+  return (
     <div className="App">
-        <div className="wrapper">
-            <p>{sum}</p>
-            <p>{filter.toString()}</p>
-            <p>{userNamesArray.toString()}</p>
-        </div>
+      <div className="wrapper">
+        {showPersonNames()}
+        {/* <p>{sum}</p>
+        <p>{filter.toString()}</p>
+        <p>{userNamesArray.toString()}</p> */}
+      </div>
     </div>
   );
 }
